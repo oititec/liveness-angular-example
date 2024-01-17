4891e1c3738fee8b22d56834a3cea5294dd6d1a3
Marcus Crisostomo <marcus.crisostomo@oititec.com.br>
2024-01-17 16:26:29 -0300

    feat: Inclusão de validação se a Appkey é válida antes de validar o ticket da API Flexível

--- src/app/flexible-api/appkey.component.ts
+++ src/app/flexible-api/appkey.component.ts
@@ -38,31 +38,41 @@ export class AppkeyFlexibleComponent implements OnInit {
     this.txtButton = 'Carregando';
     this.disabled = true;
 
-    // O código abaixo é apenas um exemplo para validar se o ticket é válido e está ativo.
-    // Não deve ser implementado no front de maneira alguma.
-    const url = `${environment.flexibleApiUrl}/bff-demo/result/${this.ticket !== '' ? this.ticket : 'undefined'}`;
-
-    const headers = new HttpHeaders({
-      'x-sub-org': '1',
-      'x-group': '1',
-      'x-branch': '1',
-    });
-
-    let result = this.http.get(url, { headers, observe: 'response' });
+    let result = this.facecaptchaService.checkAppKey(this.appkey)
 
     result.subscribe((res: any) => {
-      window.localStorage.setItem('appkey', this.appkey);
-      window.localStorage.setItem('ticket', this.ticket);
+      // O código abaixo é apenas um exemplo para validar se o ticket é válido e está ativo.
+      // Não deve ser implementado no front de maneira alguma.
+      const url = `${environment.flexibleApiUrl}/bff-demo/result/${this.ticket !== '' ? this.ticket : 'undefined'}`;
 
-      setTimeout(() => {
-        this.router.navigateByUrl('/nav-menu');
-      }, 1000);
+      const headers = new HttpHeaders({
+        'x-sub-org': '1',
+        'x-group': '1',
+        'x-branch': '1',
+      });
+
+      let ticketResult = this.http.get(url, { headers, observe: 'response' });
+
+      ticketResult.subscribe((res: any) => {
+        window.localStorage.setItem('appkey', this.appkey);
+        window.localStorage.setItem('ticket', this.ticket);
+
+        setTimeout(() => {
+          this.router.navigateByUrl('/nav-menu');
+        }, 1000);
+      },
+      (err: any) => {
+        this.txtButton = 'Continuar';
+        this.disabled = false;
+
+        this.errorMessage = JSON.stringify(err.error.message);
+      })
     },
     (err: any) => {
       this.txtButton = 'Continuar';
       this.disabled = false;
 
-      this.errorMessage = JSON.stringify(err.error.message);
+      this.errorMessage = JSON.stringify(err.error.error);
     })
   }
 
