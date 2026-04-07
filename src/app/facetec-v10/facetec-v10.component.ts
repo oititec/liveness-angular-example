c92715cf1af64ff661a207341e8bac8ba88d859b
Guilherme Batista <guilherme.batista@oititec.com.br>
2026-04-07 10:47:09 -0300

    feat: Adding basic page layouton v10 option

--- src/app/facetec-v10/facetec-v10.component.ts
+++ src/app/facetec-v10/facetec-v10.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FacecaptchaService } from '../backend/facecaptcha.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-facetec-v10',
   templateUrl: './facetec-v10.component.html',
-  styleUrls: ['./facetec-v10.component.css']
+  styleUrls: ['./facetec-v10.component.scss']
 })
-export class FacetecV10Component {
-
+export class FacetecV10Component implements OnInit {
+    FacetecLogo: string = '/assets/img/FaceTec_Logo.png';
+  
+    status: string = "";
+  
+    appkey: any;
+  
+  
+    constructor(
+      private facecaptchaService: FacecaptchaService,
+      private router: Router,
+    ) { }
+  
+    ngOnInit() {
+      // this.appkey = window.localStorage.getItem('appkey');
+      // this.status = SampleApp.status;
+  
+      // SampleApp.getProductionKey(this.facecaptchaService, this.appkey);
+    }
+  
+    showLiveness3D() {
+      // SampleApp.onLivenessCheckPressed(this.facecaptchaService, this.appkey);
+    };
+  
+    deleteAppKey() {
+      // window.localStorage.removeItem('appkey');
+      // window.localStorage.removeItem('hasLiveness');
+  
+      // this.router.navigateByUrl('/');
+    };
 }
